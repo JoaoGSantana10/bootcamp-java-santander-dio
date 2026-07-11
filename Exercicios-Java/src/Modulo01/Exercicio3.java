ab575521fe376f35805f84e31050952e51cf09bb
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-07-11 18:48:29 -0300

    feat(modulo01): Adiciona exercicio 3 Tipos Primitivos

---
+++ Exercicios-Java/src/Modulo01/Exercicio3.java
@@ -0,0 +1,18 @@
+package Modulo01;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+
+        // Tipos Primitivos
+
+        byte number = 1;
+        short N1 = 2;
+        int N2 = 3;
+        long N3 = 7L;
+        float N4 = 1.2f;
+        double N5 = 1.4d;
+        char N6 = 'a';
+        boolean N7 = true;
+
+    }
+}
