ece397ccda89035267ce1a5ffbeb05ec216ea5cc
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-07-13 20:55:52 -0300

    feat: Tabuada

---
+++ Exercicios-Java/src/Modulo01/Exercicio15.java
@@ -0,0 +1,22 @@
+package Modulo01;
+
+import java.util.Scanner;
+
+public class Exercicio15 {
+    public static void main(String[] args){
+
+        var sc = new Scanner(System.in);
+        System.out.println("Informe um número de 1 até 10: ");
+        var num = sc.nextInt();
+        System.out.printf("TABUADA DO NÚMERO: %s\n",num);
+        for (var i = 0; i <= 10; i++){
+            var tabuada = num * i;
+         System.out.printf("%s x %s = %s\n", num, i, tabuada);
+        }
+
+
+
+
+
+    }
+}
