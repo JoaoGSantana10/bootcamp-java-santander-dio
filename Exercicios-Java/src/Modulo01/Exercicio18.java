2894f4dfd105ed6e155ec723736d57c034174514
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-07-15 15:13:49 -0300

    Update Exercicio18.java

--- Exercicios-Java/src/Modulo01/Exercicio18.java
+++ Exercicios-Java/src/Modulo01/Exercicio18.java
@@ -8,19 +8,28 @@ public class Exercicio18 {
         var sc = new Scanner(System.in);
         System.out.println("Informe um número: ");
         var N1 = sc.nextInt();
-        while (true){
+        var verifica = true;
+        while (verifica){
             System.out.println("Informe um número maior que o primeiro: ");
             var N = sc.nextInt();
             if (N < N1){
-                System.out.println("Número menor que o primeiro, ele será ignorado!");
+                System.out.printf("Número menor que %s\n", N1);
                 continue;
             }
-            if (N % N1 != 0){
-                System.out.println("Resto da divisão igual a 0, deu certo!");
+            var result = N % N1;
+            verifica = result == 0;
+            System.out.printf("%s %% %s = %s\n", N, N1, result);
+
+
+
+            /* if (N % N1 != 0){
+                System.out.println("Resto da divisão igual a 0!");
             } else {
                 System.out.println("Resto da divisão diferente de 0!");
                 break;
             }
+
+             */
         }
     }
 }
