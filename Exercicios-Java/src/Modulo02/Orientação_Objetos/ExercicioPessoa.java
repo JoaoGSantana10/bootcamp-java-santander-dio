41634a4ba903c003e94652c38ca471d6e0c7bdaa
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-07-16 19:12:16 -0300

    feat: Exercicio Pessoa
    criando um objeto pessoa

---
+++ Exercicios-Java/src/Modulo02/Orientação_Objetos/ExercicioPessoa.java
@@ -0,0 +1,20 @@
+package Modulo02.Orientação_Objetos;
+
+import java.util.Scanner;
+
+public class ExercicioPessoa {
+    public static void main(String[] args){
+    var sc = new Scanner(System.in);
+    // Orientação a Objetos
+    var H = new Pessoa("João", 20);
+    //H.setName("João");
+    //H.setAge(20);
+    var M = new Pessoa("Maria", 20);
+    //M.setName("Maria");
+    //M.setAge(20);
+    System.out.println("Homem nome: " + H.getName() + "\n idade: " + H.getAge() + " anos");
+    System.out.println("Mulher nome: " + M.getName() + "\n idade: " + M.getAge() + " anos");
+
+
+    }
+}
