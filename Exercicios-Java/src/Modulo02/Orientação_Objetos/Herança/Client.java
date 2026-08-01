cb3a79d8465adcf8ed9171e51656546ef5171e01
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-08-01 19:48:37 -0300

    feat: adiciona classe Client
    Classe do cliente usada para exemplo

---
+++ Exercicios-Java/src/Modulo02/Orientação_Objetos/Herança/Client.java
@@ -0,0 +1,14 @@
+package Modulo02.Orientação_Objetos.Herança;
+
+public class Client {
+
+    private String document;
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+}
