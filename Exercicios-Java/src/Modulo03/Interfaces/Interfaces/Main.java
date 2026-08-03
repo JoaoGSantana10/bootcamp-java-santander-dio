3f788068b05a976774342e2594ea429f6d7b611f
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-08-03 12:19:58 -0300

    feat: Adiciona classe Main
    Classe onde está sendo feito a chamadas das interfaces

---
+++ Exercicios-Java/src/Modulo03/Interfaces/Interfaces/Main.java
@@ -0,0 +1,21 @@
+package Modulo03.Interfaces.Interfaces;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Computer musicPlayer = new Computer();
+        runMusic(musicPlayer);
+        runVideo(musicPlayer);
+
+    }
+
+    public static void runVideo(VideoPlayer videoPlayer){
+        videoPlayer.playVideo();
+    }
+
+    public static void runMusic(MusicPlayer musicPlayer){
+        musicPlayer.playMusic();
+    }
+
+}
