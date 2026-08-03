c1a2f5f48b1204259c2f23e75a1dd513803c0ac7
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-08-03 16:38:48 -0300

    feat: Adiciona classe email

---
+++ Exercicios-Java/src/Modulo03/Interfaces/Exercicio_Interfaces/Enviado_Mensagem/Email.java
@@ -0,0 +1,9 @@
+package Modulo03.Interfaces.Exercicio_Interfaces.Enviado_Mensagem;
+
+public class Email implements ServicoMensagem{
+
+    @Override
+    public void enviarMensagem(String mensagem) {
+        System.out.println("Enviando mensagem por E-mail: " + mensagem);
+    }
+}
