8947fe9ff742e32e444cf52b1178dbba5b3142bd
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-09-22 19:12:30 -0300

    feat: testes Relacionado ao Facade

---
+++ Exercicios-Java/src/one/digitalinnovation/gof/facade/Test.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof.facade;
+
+public class Test {
+
+    public static void main(String[] args){
+
+        // Testes relacionados ao Design Patterns: Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("João", "72250311");
+
+
+
+
+
+    }
+}
