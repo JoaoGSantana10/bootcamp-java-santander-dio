d9ecbd188d685dcc1ecd58222b90bbab194d830e
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-09-22 19:18:58 -0300

    feat: classe subsistema CepApi com singleton

---
+++ Exercicios-Java/src/subsistema2/cep/CepApi.java
@@ -0,0 +1,24 @@
+package subsistema2.cep;
+
+public class CepApi {
+
+    private static CepApi instance = new CepApi();
+
+    private CepApi(){
+        super();
+    }
+
+    public static CepApi getInstance(){
+        return instance;
+    }
+
+    public String recuperarCidade(String cep){
+        return "Brasília";
+    }
+
+    public String recuperarEstado(String cep){
+        return "DF";
+    }
+
+
+}
