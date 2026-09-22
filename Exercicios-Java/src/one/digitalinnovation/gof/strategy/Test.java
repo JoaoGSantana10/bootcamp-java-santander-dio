c251c9112c862b1f9e5fca97219b56c76a416955
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-09-22 19:17:19 -0300

    feat: testes do strategy

---
+++ Exercicios-Java/src/one/digitalinnovation/gof/strategy/Test.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.gof.strategy;
+
+public class Test {
+
+    public static void main(String[] args){
+
+        // Testes relacionados ao Design Patterns: Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+
+
+
+
+    }
+}
